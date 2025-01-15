0eb49f59ec201db8eb1e14ae558f88e1f308610e
Florin Manolache <manolachef@gmail.com>
2025-01-15 18:05:11 +0200

    Add 'approved' and 'reported' columns to threads table

---
+++ database/migrations/2025_01_15_155922_add_approved_and_reported_columns_to_threads_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('threads', function (Blueprint $table) {
+            $table->boolean('approved')->after('content')->default(false);
+            $table->boolean('reported')->after('approved')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('threads', function (Blueprint $table) {
+            $table->dropColumn(['approved', 'reported']);
+        });
+    }
+};
