70b7ab7a2eadf5ccbf7c25deab857ccd88afb91a
Florin Manolache <manolachef@gmail.com>
2025-01-16 14:32:27 +0200

    Create CategoryPolicy to restrict and manage permissions for category actions in admin panel

---
+++ app/Policies/CategoryPolicy.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+
+class CategoryPoicy
+{
+    /**
+     * Determine if the authenticated user can edit a post.
+     *
+     * @param \App\Models\User $authUser
+     * @return bool
+     */
+    public function edit(User $authUser): bool
+    {
+        // Check if the authenticated user has the "edit_category" permission
+        if ($authUser->can('edit_category')) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Determine if the authenticated user can delete a post.
+     *
+     * @param \App\Models\User $authUser
+     * @return bool
+     */
+    public function delete(User $authUser): bool
+    {
+        // Check if the authenticated user has the "delete_category" permission
+        if ($authUser->can('delete_category')) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Determine if the authenticated user can create a post.
+     *
+     * @param \App\Models\User $authUser
+     * @return bool
+     */
+    public function create(User $authUser): bool
+    {
+        // Check if the authenticated user has the "create_category" permission
+        if ($authUser->can('create_category')) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Determine if the authenticated user can create a post.
+     *
+     * @param \App\Models\User $authUser
+     * @return bool
+     */
+    public function move(User $authUser): bool
+    {
+        // Check if the authenticated user has the "move_category" permission
+        if ($authUser->can('move_category')) {
+            return true;
+        }
+
+        return false;
+    }
+}
