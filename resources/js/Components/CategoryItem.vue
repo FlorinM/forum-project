bf20da573c777b40b420ea7ad0e14265b2c40f18
Florin Manolache <manolachef@gmail.com>
2025-02-10 21:48:31 +0200

    Redesigned Home page to improve information display and organization

--- resources/js/Components/CategoryItem.vue
+++ resources/js/Components/CategoryItem.vue
@@ -1,16 +1,20 @@
 <template>
-  <li class="w-full mb-1 bg-white border border-gray-300 rounded-md transition duration-200 hover:bg-gray-200">
+  <div class="w-full mb-1 bg-white border border-gray-300 rounded-md transition duration-200 hover:bg-gray-200">
     <Link
       :href="route('categories.subcategories', category.id)"
-      class="block text-sm text-blue-600 w-full text-left p-5"
+      class="block text-sm text-blue-600 w-full text-left pl-5 pt-3 pb-3"
     >
-      {{ category.name }} (Created by User ID: {{ category.user_id }})
+      <span class="inline-block w-1 h-1 bg-blue-500 rounded-full mb-1"></span>
+      {{ category.name }}
+      <div class="text-xs mt-1 pl-2">{{ category.description }}</div>
     </Link>
-  </li>
+
+  </div>
 </template>
 
 <script setup>
 import { Link } from '@inertiajs/vue3';
+import { useFormatDate } from '@/Composables/useFormatDate';
 
 const props = defineProps({
   category: Object,
--- resources/js/Pages/Home/Home.vue
+++ resources/js/Pages/Home/Home.vue
@@ -1,25 +1,25 @@
 <template>
   <ForumLayout>
     <div class="max-w-5xl mx-auto p-5 bg-gray-100 rounded-lg shadow-md">
-      <h1 class="text-center text-4xl font-bold text-gray-800 mb-6">Categories</h1>
+      <h1 class="text-center text-xl font-bold text-gray-800 mb-6">X forum</h1>
 
       <ul class="list-none p-0">
         <li
           v-for="category in categories"
           :key="category.id"
-          :class="[
-            'w-full mb-1 bg-white border border-gray-300 rounded-md transition duration-200',
-            !category.subcategories.length ? 'hover:bg-gray-200' : ''  // Only apply hover effect if no subcategories
-          ]"
+          class="w-full mb-1 bg-white"
         >
           <Link
             :href="route('categories.subcategories', category.id)"
-            class="block text-sm text-blue-600 w-full text-left p-5"
+            class="block text-sm text-blue-700 w-full text-left p-5"
           >
-            {{ category.name }} (Created by User ID: {{ category.user_id }})
+            <h2 class="text-xl mt-3 hover:text-blue-500 transition duration-200">
+              {{ category.name }}
+            </h2>
+            <div class="text-sm pl-4">{{ category.description }}</div>
           </Link>
 
-          <ul v-if="category.subcategories && category.subcategories.length" class="pl-5 mr-2">
+          <ul v-if="category.subcategories && category.subcategories.length">
             <CategoryItem
               v-for="subcat in category.subcategories"
               :key="subcat.id"
